50f1773a7022d4166afe9ecb74f5c8d1773544be
nguyentk31 <tkn3101@gmail.com>
2024-03-30 17:04:16 +0700

    train model

--- utils/txtToCsv.py
+++ utils/txtToCsv.py
@@ -2,14 +2,11 @@ import os
 import cv2
 import csv
 
-# Convert txt location file to csv file
-def convert_txt_to_csv(filepath):
-  # Read file name and change to new extension
-  filename, _ = os.path.splitext(filepath)
-  csv_filename = f'{filename}.csv'
+# Convert txt location file to dataset csv file
+def convert_txt_to_csv(locationfile):
 
  # Read each line in txt file and write csv file
-  with open(filepath, 'r') as input, open(csv_filename, 'w', newline='') as output:
+  with open(locationfile, 'r') as input, open('dataset.csv', 'w', newline='') as output:
     writer = csv.writer(output)
     for index, line in enumerate(input):
       # Set dataset type
@@ -32,6 +29,5 @@ def convert_txt_to_csv(filepath):
         xMin, yMin, xMax, yMax = x/img_width, y/img_height, (x+width)/img_width, (y+height)/img_height
         writer.writerow([data_type, imageURL, label, xMin, yMin, '', '', xMax, yMax, '', ''])
 
-# Example usage
 if __name__ == '__main__':
   convert_txt_to_csv('./location.txt')
