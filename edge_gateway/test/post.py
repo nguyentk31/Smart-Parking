707f1a4e94cfa91cff5719dde9e219426f36cad6
hoanglong2206 <21522307@gm.uit.edu.vn>
2024-04-30 23:17:50 +0700

    update

--- edge_gateway/test/post.py
+++ edge_gateway/test/post.py
@@ -2,6 +2,7 @@ import cv2
 import requests
 
 def post_data(API, image, direction, lisence_plate):
+  API_test = 'http://localhost:8800/data'
   image_byte = cv2.imencode('.jpg', image)[1].tobytes()
   files = {'log': ('image.jpg', image_byte, 'image/jpg')}
   data = {
@@ -10,7 +11,13 @@ def post_data(API, image, direction, lisence_plate):
     'lp_part2': lisence_plate[1]
   }
   try:
-    rsp = requests.post(API, data=data, files=files)
+    # rsp = requests.post(API_test, data=data, files=files)
+    if direction == 'in':
+      API += 'createParking'
+      rsp = requests.post(API, data=data, files=files)
+    else:
+      API += 'updateParkingCheckOut'
+      rsp = requests.patch(API, data=data, files=files)
   except Exception as e:
     print('Error:')
     print(e)
@@ -18,8 +25,8 @@ def post_data(API, image, direction, lisence_plate):
     print(rsp.text)
 
 if __name__ == '__main__':
-  API = 'http://localhost:8800/data'
-  image = cv2.imread('lp_dataset/0000_00532_b.jpg')
+  API_SERVER = 'http://localhost:3000/api/parking/'
+  image = cv2.imread('lp_dataset/anh1.jpg')
   direction = 'in'
-  lisence_plate = ['34T2', '36354']
-  post_data(API, image, direction, lisence_plate)
+  lisence_plate = ['51A9', '224466']
+  post_data(API_SERVER, image, direction, lisence_plate)
